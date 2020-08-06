0bee980bc159abc1d5b030879477c136d6f3a051
BunnyTheLifeguard <36411819+BunnyTheLifeguard@users.noreply.github.com>
2020-08-06 14:48:20 +0200

    Added buttons to dices w/ console msgs

--- lib/main.dart
+++ lib/main.dart
@@ -22,14 +22,18 @@ class DicePage extends StatelessWidget {
       child: Row(
         children: <Widget>[
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
+            child: FlatButton(
+              onPressed: () {
+                print('Left Dice');
+              },
               child: Image.asset('images/dice1.png'),
             ),
           ),
           Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
+            child: FlatButton(
+              onPressed: () {
+                print('Right Dice');
+              },
               child: Image.asset('images/dice1.png'),
             ),
           ),
